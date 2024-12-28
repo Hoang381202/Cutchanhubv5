f164bb0ec305bb33e03c0e1d4f7295640240994b
Hoang381202 <phuonglien3802@gmail.com>
2024-12-29 04:13:00 +0700

    Update cutchanv5,.lua

--- cutchanv5,.lua
+++ cutchanv5,.lua
@@ -8,8 +8,6 @@ game:GetService("StarterGui"):SetCore("SendNotification",{
 })
 if table.find(TablePlace,game.PlaceId) then
     _G.Game = "BF"
-    _G.Key = "DVHDZ-NNLXG-772EH"
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/BF-New.lua"))()
 else
     game.Players.LocalPlayer:Kick("địt mẹ ngu game này dell hỗ trợ")
